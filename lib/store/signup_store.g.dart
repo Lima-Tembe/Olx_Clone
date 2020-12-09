96a48d87b731ca74ecfd2772461f68aa76fba813
Lima <limatembe44@gmail.com>
2020-12-09 21:42:56 +0200

    fixed mobx build runner for the page_store and signup_store

---
+++ lib/store/signup_store.g.dart
@@ -0,0 +1,235 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signup_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$SignupStore on _SignupStoreBase, Store {
+  Computed<bool> _$nameValidComputed;
+
+  @override
+  bool get nameValid =>
+      (_$nameValidComputed ??= Computed<bool>(() => super.nameValid,
+              name: '_SignupStoreBase.nameValid'))
+          .value;
+  Computed<bool> _$emailValidComputed;
+
+  @override
+  bool get emailValid =>
+      (_$emailValidComputed ??= Computed<bool>(() => super.emailValid,
+              name: '_SignupStoreBase.emailValid'))
+          .value;
+  Computed<bool> _$phoneValidComputed;
+
+  @override
+  bool get phoneValid =>
+      (_$phoneValidComputed ??= Computed<bool>(() => super.phoneValid,
+              name: '_SignupStoreBase.phoneValid'))
+          .value;
+  Computed<bool> _$passValidComputed;
+
+  @override
+  bool get passValid =>
+      (_$passValidComputed ??= Computed<bool>(() => super.passValid,
+              name: '_SignupStoreBase.passValid'))
+          .value;
+  Computed<bool> _$confirmPassValidComputed;
+
+  @override
+  bool get confirmPassValid => (_$confirmPassValidComputed ??= Computed<bool>(
+          () => super.confirmPassValid,
+          name: '_SignupStoreBase.confirmPassValid'))
+      .value;
+  Computed<bool> _$isFormValidComputed;
+
+  @override
+  bool get isFormValid =>
+      (_$isFormValidComputed ??= Computed<bool>(() => super.isFormValid,
+              name: '_SignupStoreBase.isFormValid'))
+          .value;
+  Computed<Function> _$signupPressedComputed;
+
+  @override
+  Function get signupPressed =>
+      (_$signupPressedComputed ??= Computed<Function>(() => super.signupPressed,
+              name: '_SignupStoreBase.signupPressed'))
+          .value;
+
+  final _$nameAtom = Atom(name: '_SignupStoreBase.name');
+
+  @override
+  String get name {
+    _$nameAtom.reportRead();
+    return super.name;
+  }
+
+  @override
+  set name(String value) {
+    _$nameAtom.reportWrite(value, super.name, () {
+      super.name = value;
+    });
+  }
+
+  final _$emailAtom = Atom(name: '_SignupStoreBase.email');
+
+  @override
+  String get email {
+    _$emailAtom.reportRead();
+    return super.email;
+  }
+
+  @override
+  set email(String value) {
+    _$emailAtom.reportWrite(value, super.email, () {
+      super.email = value;
+    });
+  }
+
+  final _$phoneAtom = Atom(name: '_SignupStoreBase.phone');
+
+  @override
+  String get phone {
+    _$phoneAtom.reportRead();
+    return super.phone;
+  }
+
+  @override
+  set phone(String value) {
+    _$phoneAtom.reportWrite(value, super.phone, () {
+      super.phone = value;
+    });
+  }
+
+  final _$passAtom = Atom(name: '_SignupStoreBase.pass');
+
+  @override
+  String get pass {
+    _$passAtom.reportRead();
+    return super.pass;
+  }
+
+  @override
+  set pass(String value) {
+    _$passAtom.reportWrite(value, super.pass, () {
+      super.pass = value;
+    });
+  }
+
+  final _$confirmPassAtom = Atom(name: '_SignupStoreBase.confirmPass');
+
+  @override
+  String get confirmPass {
+    _$confirmPassAtom.reportRead();
+    return super.confirmPass;
+  }
+
+  @override
+  set confirmPass(String value) {
+    _$confirmPassAtom.reportWrite(value, super.confirmPass, () {
+      super.confirmPass = value;
+    });
+  }
+
+  final _$isLoadingAtom = Atom(name: '_SignupStoreBase.isLoading');
+
+  @override
+  bool get isLoading {
+    _$isLoadingAtom.reportRead();
+    return super.isLoading;
+  }
+
+  @override
+  set isLoading(bool value) {
+    _$isLoadingAtom.reportWrite(value, super.isLoading, () {
+      super.isLoading = value;
+    });
+  }
+
+  final _$_signupAsyncAction = AsyncAction('_SignupStoreBase._signup');
+
+  @override
+  Future<void> _signup() {
+    return _$_signupAsyncAction.run(() => super._signup());
+  }
+
+  final _$_SignupStoreBaseActionController =
+      ActionController(name: '_SignupStoreBase');
+
+  @override
+  void setName(String value) {
+    final _$actionInfo = _$_SignupStoreBaseActionController.startAction(
+        name: '_SignupStoreBase.setName');
+    try {
+      return super.setName(value);
+    } finally {
+      _$_SignupStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setEmail(String value) {
+    final _$actionInfo = _$_SignupStoreBaseActionController.startAction(
+        name: '_SignupStoreBase.setEmail');
+    try {
+      return super.setEmail(value);
+    } finally {
+      _$_SignupStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setPhone(String value) {
+    final _$actionInfo = _$_SignupStoreBaseActionController.startAction(
+        name: '_SignupStoreBase.setPhone');
+    try {
+      return super.setPhone(value);
+    } finally {
+      _$_SignupStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setPass(String value) {
+    final _$actionInfo = _$_SignupStoreBaseActionController.startAction(
+        name: '_SignupStoreBase.setPass');
+    try {
+      return super.setPass(value);
+    } finally {
+      _$_SignupStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setconfirmPass(String value) {
+    final _$actionInfo = _$_SignupStoreBaseActionController.startAction(
+        name: '_SignupStoreBase.setconfirmPass');
+    try {
+      return super.setconfirmPass(value);
+    } finally {
+      _$_SignupStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+name: ${name},
+email: ${email},
+phone: ${phone},
+pass: ${pass},
+confirmPass: ${confirmPass},
+isLoading: ${isLoading},
+nameValid: ${nameValid},
+emailValid: ${emailValid},
+phoneValid: ${phoneValid},
+passValid: ${passValid},
+confirmPassValid: ${confirmPassValid},
+isFormValid: ${isFormValid},
+signupPressed: ${signupPressed}
+    ''';
+  }
+}
